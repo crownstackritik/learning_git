d8200a7613bbf3c0585011749138ff6f915c6369
login at work <ritik@example.com>
2025-01-08 18:28:10 +0530

    change in cpp file

--- file1.cpp
+++ file1.cpp
@@ -3,7 +3,8 @@ using namespace std;
 int main(){
     int a,b;
     cin>>a>>b;
-    cout<<"value of a "<<a<<endl;
-    cout<<"value of a "<<a<<endl;
+    cout<<"value of a  :"<<a<<endl;
+    cout<<"value of b  :"<<b<<endl;
+    cout<<"value of a%b   :"<<a%b<<endl;
     return 0;
 }
