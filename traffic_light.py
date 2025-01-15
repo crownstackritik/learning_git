7634255e6c5bf31649b0e0e7f9258c44cf10335e
Ritk <ritiksihag241@gmail.com>
2025-01-15 16:35:51 +0530

    traffic light

---
+++ traffic_light.py
@@ -0,0 +1,23 @@
+"""a=4
+b=-7
+c=6
+d=-5
+print(a%b)
+print(b%a)
+print(b%d)
+print(a%c)
+#as in tutorial said -/+ is only gives us the nesgative modulo value but here -/- is also giving - value
+#learning to take input
+name=str(input("name:"))
+age=int(input("age:"))
+
+print("My name is ",name,"and I am ",age," years old")"""
+color=input("light color:")
+if(color=="red"):
+    print("stop")
+elif(color=="yellow"):
+    print("look")
+elif(color=="green"):
+    print("go")
+else:
+    print("light is broken")    
