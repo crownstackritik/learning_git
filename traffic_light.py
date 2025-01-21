de2782944f1e81f347e4097620bd7bef6f683899
crownstackritik <ritik@crownstack.com>
2025-01-21 15:34:46 +0530

    Update traffic_light.py

--- traffic_light.py
+++ traffic_light.py
@@ -1,17 +1,3 @@
-"""a=4
-b=-7
-c=6
-d=-5
-print(a%b)
-print(b%a)
-print(b%d)
-print(a%c)
-#as in tutorial said -/+ is only gives us the nesgative modulo value but here -/- is also giving - value
-#learning to take input
-name=str(input("name:"))
-age=int(input("age:"))
-
-print("My name is ",name,"and I am ",age," years old")"""
 color=input("light color:")
 if(color=="red"):
     print("stop")
